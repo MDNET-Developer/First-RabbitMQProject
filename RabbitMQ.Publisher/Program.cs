ece6d13b036f647e3f0f69b1a442b956c3fd630c
MDNET-Developer <qarabagli.murad@mail.ru>
2023-02-10 11:13:55 +0400

    First-RabbitMQProject

---
+++ RabbitMQ.Publisher/Program.cs
@@ -0,0 +1,45 @@
+﻿using RabbitMQ.Client;
+using System;
+using System.Text;
+
+namespace RabbitMQ.Publisher
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            //Ilk once RabbitMQ-e baglanmaq ucun ConnectionFactory class-ni newlemek lazimdir.
+            var connectionFactory = new ConnectionFactory();
+
+            connectionFactory.Uri = new Uri("amqps://ckwazfzf:l-f-a2qMhowYvCYK_8PmYtzSbcVEa06f@woodpecker.rmq.cloudamqp.com/ckwazfzf");
+
+            //Bir dene baglanti acaq
+            using var createConnection = connectionFactory.CreateConnection();
+
+            //Bir dene kanal yaradaq bu baglanti uzerinden
+            var channel = createConnection.CreateModel();
+
+            channel.QueueDeclare("donkey-queue", true, false, false);
+            //("quyruq adi", bool durable = true/false(əgər false etsək butun quyruqlar yaddasda tutulur,əgər biz tətbiqə restart verəsək itəcək bu datalar. true versək əgər fiziki olaraq yadda saxlanilir və tətbiqə restart versək belə orada olacaq) ,bool exclusive =true/false (eger biz buna true yazsaq bu quyruq  yalniz bizim yaratdigimiz kanala qosulacaq,əgər biz istəsək subcriber tərəfində fərqli bir kanalada bağlansın o zaman false qeyd edəcəyik),bool autoDelete = true/false (əgər true qeyd etsək bizim son subcribe-mız çıxan zaman quyruq avtomatik silinəcək. False olsa orada saxlanılacaq)
+            Console.Write("Mesajınızı daxil edin---> ");
+            string message = Console.ReadLine();
+            for (int i = 1; i < 56; i++)
+            {
+                string allMessage = message + i.ToString();
+                //Gonderidiyimiz mesajlar byte tipinde gonderilir. Buna gorede byte-lasdirma etdik
+                var messageBody = Encoding.UTF8.GetBytes(allMessage);
+                //Ara vasiteci olaraq bizde exchange olmadigi ucun "" veya string.Empty kimi qeyd edirik.
+                //Exchange isletmeydimiz ucun default exchange den istifade olunur ki, burada quyruq adi daha sonra qeyd olunur
+                channel.BasicPublish(string.Empty, "donkey-queue", null, messageBody);
+                System.Threading.Thread.Sleep(1555);
+                Console.WriteLine($"{i})'{allMessage}'  - mesajı ugurla gonderildi :)");
+                Console.WriteLine("-----------------------------------------------");
+            }
+
+            
+            Console.ReadLine();
+
+        }
+    }
+}
---
+++ RabbitMQ.Subcriber/Program.cs
@@ -0,0 +1,58 @@
+﻿using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System;
+using System.Text;
+
+namespace RabbitMQ.Subcriber
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.ForegroundColor
+          = ConsoleColor.Green;
+            //Ilk once RabbitMQ-e baglanmaq ucun ConnectionFactory class-ni newlemek lazimdir.
+            var connectionFactory = new ConnectionFactory();
+
+            connectionFactory.Uri = new Uri("amqps://ckwazfzf:l-f-a2qMhowYvCYK_8PmYtzSbcVEa06f@woodpecker.rmq.cloudamqp.com/ckwazfzf");
+
+            //Bir dene baglanti acaq
+            using var createConnection = connectionFactory.CreateConnection();
+
+            //Bir dene kanal yaradaq bu baglanti uzerinden
+            var channel = createConnection.CreateModel();
+
+
+            //Burda kicik bir incelik var  - Bunu istesek sile bilerik, amma saxlamaqda problem yoxdur. Eger bizim publisher da quyruq yaradildigina aid melumatimiz varsa sile bilerik.
+            //Hem publisher hemde subcriber terefde quyruq yaradirsansa eger icerisinde daxil etdiyimiz butun parametrler eyni olmalidir. Bunlar - "donkey-queue", true, false, false
+            channel.QueueDeclare("donkey-queue", true, false, false);
+            
+            //Musteri/abuneci yaradir
+            var subcriber = new EventingBasicConsumer(channel);
+
+            //bu subcriber hansi quyruq ile isleyecek - 
+            //autoAck- true yazsaq eger  biz bu mesaj gonderende subcribere catanda avtomatik quyruqdan silinir. False yazsaq eger silinme emeliyyati bizim elimizde olur
+            channel.BasicConsume("donkey-queue", false, subcriber);
+
+
+            //global true versek misal ucun 2 subcriber var 5 dene mesaj gonderirik 2 birine 3 birine verecek yeni ki tek seferde 5 mesaj getmelidir her ikisine, 6 desek 3 ona 3 buna bele bir sey. Yox eger false edek her biri ucun gonderecek
+            channel.BasicQos(0, 1, false);
+            //SUbcribere mesaj gelende bu event isleyecek(Recieved)
+            subcriber.Received += (object sender, BasicDeliverEventArgs e) =>
+            {
+               
+                var receivedMessage = Encoding.UTF8.GetString(e.Body.ToArray());
+                Console.WriteLine($"Sizin yeni mesajınız var - {receivedMessage}");
+                System.Threading.Thread.Sleep(1555);
+                //multiple = true/false ( true olsa eger islenmis amma gonderilmesmis mesajlar rabbitmq e melumat verer)
+                channel.BasicAck(e.DeliveryTag,false);
+            };
+            
+            Console.ReadLine();
+        }
+
+    }
+}
+
+
